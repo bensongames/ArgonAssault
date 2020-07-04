f546ce9459717d1818c97294b603b0d30e220058
Darren Benson <darren.benson@deskpoint.com>
2020-07-04 14:39:56 +0100

    Complete C# Unity Developer 3D: 84 Unity's Waypoint Utility Scripts

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel1", 10f);
+    }
+
+void LoadLevel1()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+}
