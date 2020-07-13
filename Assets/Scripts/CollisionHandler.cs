575a9108f095eb7cece6b096d167da3cd33362d6
Darren Benson <darren.benson@deskpoint.com>
2020-07-14 00:46:57 +0100

    100: Enable a GameObject from C#

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,18 +1,27 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+
+    [Tooltip("In seconds")][SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("Particle Effects")][SerializeField] GameObject deathFX;
+
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
     }
 
-
     private void StartDeathSequence()
     {
-        print("Boom!");
-        SendMessage("OnPlayerDeath");
+        deathFX.SetActive(true);
+        Invoke("ReloadLevel", levelLoadDelay);        
+    }
+
+    private void ReloadLevel() // String referenced
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 }
